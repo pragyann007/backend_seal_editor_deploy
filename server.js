c5863720fc7fe902862b464b7a89f3ec7cd17509
pragyann007 <pragyanthapaliya027@gmail.com>
2025-10-23 14:33:15 +0545

    cors fixed again

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://sealeditor.netlify.app/",
+    origin:"https://sealeditor.netlify.app",
     credentials:true
 }))
 
